d69fecfefb92aa97a48116af7be57688fb621560
vilmaruth <vilmaruthtito@gmail.com>
2015-06-03 18:33:47 -0300

    seguimiento documentacion de requisitos, programacion sistema embebido en arduino

--- Codigo/italarma/italarma.ino
+++ Codigo/italarma/italarma.ino
@@ -2,55 +2,144 @@
 inicializacion de la variable necesarias
 
 */
-
 #include <macro.h>
+#include <SoftwareSerial.h>
 
-char rxchar;          //variable que recibe los datos del bluetooth
-int estadoSysAlarma;  // estado del sistema de alarma: 0-DESACTIVADO, 1-ACTIVADO, 2-BATERIA, 3-BATERIA_CRITICA
-int pinSensorApertura;//se debe controlar que esta HIGH al estar activada la alarma.
+char msj;          //variable que recibe los datos del bluetooth
+int estadoSysAlarma;  // estado del sistema de alarma: ver <macro.h>
 
-//  configurar el arduino
+// estado de los distintos sensores
+int estadoSensorMovimiento;
+int estadoSensorLuces;
+int estadoSensorAperturas;
+int estadoSensorGases; 
+
+//codigo de seguridad del sistema de alarma
+int codigo;
+// 
+int tiempoDeEspera;
+int crono=0;
+// variable necesaria para la comunicacion bluetooth
+SoftwareSerial blue(PINRX,PINTX);
+
+//  configuracion inicial del arduino
 void setup(){
-    Serial.begin(9600);
-    estadoSysAlarma=0;
-    pinMode(pinSensorApertura,INPUT);
+    blue.begin(9600);
+    estadoSysAlarma=DESACTIVADO;
+    tiempoDeEspera=10000;//10 segundos, debe ser proporcional a la distancia del tablero a la puerta principal
 }
 
 // programa  principal
 void loop(){
-//  long pinSApertura=
-
-  if (estadoSysAlarma == 1){//sistema de alarma activado tiene que controlar el estado de los sensores.
+  
+  
+  if (estadoSysAlarma == ACTIVADO){//sistema de alarma activado tiene que controlar el estado de los sensores.
     // COMPROBAR ACTIVACION SENSOR DE MOVIMIENTO
+    if (estadoSensorMovimiento){
+    
+    }
     // COMPROBAR ACTIVACION SENSOR DE GASES
-    
+    if (estadoSensorGases){}
     // COMPROBAR ACTIVACION APERTURA DE ABERTURAS
-    
-    if (pinSensorApertura==LOW){//existe alguna abertura abierta que corta el flujo de electricidad.
-      //activar alarma.
+    if (estadoSensorAperturas){
+      if (digitalRead(PINSAPERTURA0)==LOW){//La puerta principal se encuentra abierta
+        //activar bocina.
+        delay(1000);
+        crono++;
+        if (crono==tiempoDeEspera){
+          //activar la bocina
+        }
+      }
+      if (digitalRead(PINSAPERTURA1)==LOW){//existe alguna abertura abierta que corta el flujo de electricidad.
+        //activar bocina.
+      }
+    }
+    // ACTIVACION DE SENSOR DE LUZ
+    if (estadoSensorLuces){
     }
   }
-  if (estadoSysAlarma== 0){// Sistema desactivado, 
-   //SEÑAL BLUETOOTH
-    if( Serial.available() )// bluetooth
-    {
-      // Leer un byte y colocarlo en variable
-      // los mensajes de entrada  pueden ser:
-      // - activacion de sistema de alarmas
-      // - desactivacion de sistema de alarmas
-      // - activar sistema de alarma  por ambientes(sugeta a la cantidad de ambientes)
-      // - desactivacion de sistema de alarma por ambientes(idem anterior)
-       Serial.read();
-    }
-    // TABLERO. protocolo I2C
-    
-    // mensajes de entrada:
-    // - activacion de sistema de alarmas
-    // - desactivacion de sistema de alarmas
-    //SENSOR DE LUCES
+  if (estadoSysAlarma== DESACTIVADO){// Sistema desactivado, 
+    //controlar estado de bateria
+  }
+  if(estadoSysAlarma==BATERIAOPTIMA){
+  
+  }
+  if(estadoSysAlarma== BATERIACRITICA){
+  
   }
   // REVISION DE LA BATERIA
   controlarEstadoBateria();  
+  //SEÑAL BLUETOOTH. Procesamiento de mensajes.
+  if( blue.available()){
+    // Leer un byte y colocarlo en variable
+    // los mensajes de entrada  pueden ser:
+    // - activacion de sistema de alarmas
+    // - desactivacion de sistema de alarmas
+    msj = blue.read();
+    switch(msj){
+      case ACTIVARSYS:
+        estadoSysAlarma= ACTIVADO;
+        estadoSensorMovimiento=HIGH;
+        estadoSensorLuces=HIGH;
+        estadoSensorGases=HIGH;
+        estadoSensorAperturas=HIGH;
+      break;
+      case DESACTIVARSYS:
+        estadoSysAlarma= DESACTIVADO;
+        estadoSensorMovimiento=LOW;
+        estadoSensorLuces=LOW;
+        estadoSensorGases=LOW;
+        estadoSensorAperturas=LOW;
+      break;
+      case ACTIVARSENSORMOVIMIENTO:
+        if (estadoSysAlarma==ACTIVADO){
+          estdoSensorMovimiento=HIGH;
+        }
+      break;
+      case DESACTIVARSENSORMOVIMIENTO:  
+        if (estadoSysAlarma==ACTIVADO){
+          estadoSensorMovimiento=LOW;
+        }
+      break;
+      case ACTIVARSENSORLUCES:
+        if (estadoSysAlarma==ACTIVADO){
+          estadoSensorLuces=HIGH;
+        }
+      break;
+      case DESACTIVARSENSORLUCES:
+        if (estadoSysAlarma==ACTIVADO){
+          estadoSensorLuces=LOW;
+        }
+      break;
+      case ACTIVARSENSORGASES:
+        if (estadoSysAlarma==ACTIVADO){
+          estadoSensorGases=HIGH;
+        }
+      break;
+      case DESACTIVARSENSORGASES: 
+        if (estadoSysAlarma==ACTIVADO){
+          estadoSensorGases=LOW;
+        }
+      break;
+      case ACTIVARSENSORAPERTURA:
+        if (estadoSysAlarma==ACTIVADO){
+          estadoSensorAperturas=HIGH;
+        }
+      break;
+      case DESACTIVARSENSORAPERTURA:
+        if (estadoSysAlarma==ACTIVADO){
+          estadoSensorAperturas=LOW;
+        }
+      break;
+      default:
+    }
+    
+  }
+  // TABLERO. protocolo I2C
+  // mensajes de entrada:
+  // - activacion de sistema de alarmas
+  // - desactivacion de sistema de alarmas
+ 
 }//fin loop().
 
 
@@ -58,23 +147,8 @@ void loop(){
   void controlarEstadoBateria(){
     
   }
-  
-  boolean leer_voltios(int pinAnalog){
-    float  voltios;
-    voltios= (analogRead(pinAnalog))*(0,004887585533)*4.9000;
-    return voltios<=6.60;
-  }
-    /*Para una referencia de 3.3 Voltios
-    void leer_voltios()
-    {
-    float  voltios;
-    voltios= (analogRead(A0))*0,003225806452*4.9000;
-    if (voltios<=6.60){
-      digitalWrite(5, HIGH);
-    }
-}*/
-
 // COMPROBAR ACTIVACION SENSOR DE MOVIMIENTO
 // COMPROBAR ACTIVACION SENSOR DE GASES
 // COMPROBAR ACTIVACION APERTURA DE ABERTURAS
   
+
---
+++ Codigo/italarma/lib/macro.h
@@ -0,0 +1,50 @@
+/*
+En esta clase se definiran las macros utilizadas  en el programa arduino para hacer referencia
+a estados del sistema, pines digitales, analogicos y PWM(modulacion de impulsos pro frecuencia),
+mensajes del sistema.
+*/
+
+#ifndef __MACRO_H
+#define __MACRO_H
+
+// ESTADOS DEL SISTEMA DE ALARMAS
+#define  DESACTIVADO  1000
+#define  ACTIVADO  1001
+#define  BATERIAOPTIMA  1002
+#define  BATERIACRITICA  1003
+
+//DEFINICION DE PINES DIGITALES
+#define PIN   1
+#define PINRX   2  //comunicacion bluetooth
+#define PINTX   3  //comunicacion bluetooth
+#define PINSAPERTURA0   4
+#define PINSAPERTURA1   5
+#define PIN   6
+#define PIN   7
+#define PIN   8
+#define PIN   9
+#define PIN   10
+#define PIN   11
+#define PIN   12
+#define PIN   13
+
+// DEFINICION DE PINES ANALOGICOS NO ES NECESARIO. A0, A1, A2, A3, A4, A5.
+
+//DEFINICION DE MENSAJES 
+#define ACTIVARSYS A
+#define DESACTIVARSYS a
+
+#define ACTIVARSENSORMOVIMIENTO M
+#define DESACTIVARSENSORMOVIMIENTO m
+
+#define ACTIVARSENSORLUCES L
+#define DESACTIVARSENSORLUCES l
+
+#define ACTIVARSENSORGASES G
+#define DESACTIVARSENSORGASES g
+
+#define ACTIVARSENSORAPERTURA V
+#define DESACTIVARSENSORAPERTURA v
+
+
+#endif
